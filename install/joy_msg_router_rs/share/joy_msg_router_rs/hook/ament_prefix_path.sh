a60039b487ec0b31ac85f83c0c41d9d9ba7c058a
Masahiro Wada <argon.argon.argon@gmail.com>
2025-06-01 03:48:02 +0000

    chore: remove files

--- install/joy_msg_router_rs/share/joy_msg_router_rs/hook/ament_prefix_path.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value AMENT_PREFIX_PATH "/workspaces/dev-joy-msg-router-rs/src/joy_msg_router_rs/install/joy_msg_router_rs"
